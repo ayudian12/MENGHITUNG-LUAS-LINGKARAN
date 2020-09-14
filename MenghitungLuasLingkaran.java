c4970e184c8b793b32ffac2eb1a035ab92ce25b6
ayudian12 <70583611+ayudian12@users.noreply.github.com>
2020-09-14 14:24:01 +0700

    Add files via upload

---
+++ MenghitungLuasLingkaran.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author ASUS A407M
+ */
+public class MenghitungLuasLingkaran {
+ public static void main(String[] args) {
+     double phi = 3.14;
+     int r =21;
+     double L = phi * (r * r);
+     
+     
+     System.out.println("===PROGRAM MENGHITUNG LUAS LINGKARAN===");
+     System.out.println("phi\t= "+phi+"cm");
+     System.out.println("r\t= "+r+"cm");
+     System.out.println("L\t= phi*(r*r)" );
+     System.out.println("\t= "+L+"cm2"); 
+     
+ }
+         
+    
+}
